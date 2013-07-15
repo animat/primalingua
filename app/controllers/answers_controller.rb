3af5dd3a23477ee83fac8baa992eeb7a68ab7e3b
Colin Angevine <colinangevine@gmail.com>
2013-07-15 16:11:37 -0400

    Notification and StudentProgress are created when a student completes a lesson

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,7 +8,12 @@ class AnswersController < ApplicationController
     @answer = Answer.where(question_id: params[:answer][:question_id], student_id: params[:answer][:student_id]).first_or_create!
     @answer.content = params[:answer][:content]
   	if @answer.save
-  	  render :json => @answer, :status => :ok
+      @student = Student.find(params[:answer][:student_id])
+      @lesson = @answer.lesson
+      if Question.where(:lesson => l).count == Answer.by_student(s).in_lesson(l).count
+        create_progress_and_notification(@lesson, @student, @student.teacher, "completed")
+      end
+      render :json => @answer, :status => :ok
   	else
   	  render :json => @answer.errors, :status => :unprocessable_entity
   	end
@@ -19,6 +24,9 @@ class AnswersController < ApplicationController
     @student = Student.find(answer_params[:student_id])
     if is_student_or_teacher_for(0, @student)
       if @answer.update(answer_params)
+        #if **this feedback is for a whole lesson**
+          #create_progress_and_notification(@lesson, @student, @student, "graded")
+        #end
         render :json => @answer, :status => :ok
       else
         render :json => @answer.errors, :status => :unprocessable_entity
@@ -31,5 +39,10 @@ class AnswersController < ApplicationController
   private
   def answer_params
   	params.require(:answer).permit(:student_id, :question_id, :content, :id, :feedback, :feedback_status)
+  end
+
+  def create_progress_and_notification(les, stu, recip, stat)
+    @sp = StudentProgress.where(:student_id => s, :lesson_id => l, :status => stat).first_or_create!
+    @n = Notification.where(:notifiable => @sp, :recipientable => recip).first_or_create!
   end
 end
