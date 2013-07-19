382c7cc3f7e7fccea1ce22ce54a3b302852f5b83
Colin Angevine <colinangevine@gmail.com>
2013-07-18 22:43:39 -0400

    Editing lesson plans now works

--- app/controllers/lesson_plans_controller.rb
+++ app/controllers/lesson_plans_controller.rb
@@ -69,6 +69,6 @@ class LessonPlansController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def lesson_plan_params
-      params.require(:lesson_plan).permit(:type, :content)
+      params.require(:lesson_plan).permit(:lesson_id, :content)
     end
 end
