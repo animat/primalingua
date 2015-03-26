77bf1a52885fa0b571749f4481b6f7a30c4ef5cb
Colin Angevine <info@linguazone.com>
2015-03-26 10:28:34 -0400

    Resolve error when marking a notification as read when it does not exist

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,11 +3,13 @@ class NotificationsController < ApplicationController
     if params[:question_id] and params[:student_id]
       @answer = Answer.where(question_id: params[:question_id], student_id: params[:student_id]).first
       @notification = Notification.where(notifiable_type: "Answer", notifiable_id: @answer.id, unread: true).first
-      @notification.unread = false
-      if @notification.save
-        render json: "Success", status: :ok
-      else
-        render json: "Failure: unable to update notification", status: :unprocessable_entity
+      unless @notification.nil?
+        @notification.unread = false
+        if @notification.save
+          render json: "Success", status: :ok
+        else
+          render json: "Failure: unable to update notification", status: :unprocessable_entity
+        end
       end
     else
       render json: "Failure: invalid parameters", status: :unprocessable_entity
