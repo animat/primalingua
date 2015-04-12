87f3d91d6e461c16ac98957b1200e7efb1c2da39
Colin Angevine <info@linguazone.com>
2015-04-12 10:40:35 -0400

    Corrected glitch with reading nil notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,9 @@ class NotificationsController < ApplicationController
     if params[:question_id] and params[:student_id]
       @answer = Answer.where(question_id: params[:question_id], student_id: params[:student_id]).first
       @notification = Notification.where(notifiable_type: "Answer", notifiable_id: @answer.id, unread: true).first
-      unless @notification.nil?
+      if @notification.nil?
+        render json: "Failure: notification not found", status: :unprocessable_entity
+      else
         @notification.unread = false
         if @notification.save
           render json: "Success", status: :ok
